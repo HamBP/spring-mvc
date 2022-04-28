fb708de93d8ea0b5639982215ed24be207b7473d
algosketch <sjy_9484@naver.com>
2022-04-29 00:53:01 +0900

    add logger

---
+++ src/main/java/org/algosketch/springmvc/basic/LogTestController.java
@@ -0,0 +1,24 @@
+package org.algosketch.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.debug(" debug log={}", name);
+        log.trace(" trace log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error(" error log={}", name);
+
+        return "ok";
+    }
+}
